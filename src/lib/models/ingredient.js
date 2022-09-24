d7e0103222fdd74cb5646079428e6471ea1b108a
angelina <angelina.stone@dxdiag.co.za>
2022-09-24 15:56:42 +0200

    models created/updated

---
+++ src/lib/models/ingredient.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+  const IngredientSchema = new mongoose.Schema({
+    Name: {
+      type: String,
+      required: true,
+    },
+    Mass: {
+      type: String,
+      required: true,
+    },
+    Quantity: {
+        type: mongoose.Schema.Types.Number,
+        required: false,
+        default: 1
+      },
+      Price: {
+        type: mongoose.Schema.Types.Decimal128,
+        required: true,
+        default: 0.00
+      },
+    Barcode:{
+      type: mongoose.Schema.Types.ObjectId,
+        required: true,
+      }
+  });
+
+module.exports = mongoose.model("Ingredient", IngredientSchema);
+
+
